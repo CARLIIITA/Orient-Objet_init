4b9f126ba64b8bcd881c2a2eb01eba7622b7adeb
unknown <carla.elatre@gmail.com>
2024-03-11 16:52:00 +0100

    commit

--- Voiture.php
+++ Voiture.php
@@ -2,22 +2,39 @@
 
 class Voiture
 {
+    private string $unite = "km";
     public float $vitesse;
     public string $marque;
     public float $temps;
 
     public function calculerDistance()
 {
-    //'THIS' VA PERMETTRE DE POINTER UN OBJET QUI N'A PAS ENCORE ETE INTANCER
-    $distance = $this->vitesse * $this->temps;
-    return $distance;
+    //'THIS' VA PERMETTRE DE POINTER UN OBJET QUI N'A PAS ENCORE ETE INTANCER   
+    return $this->vitesse * $this->temps . $this ->unite;
 } 
-
+static function getInfo()
+{
+  return "inf";  
 }
-$voiture = new Voiture ();
-$voiture->vitesse = 55;
-$voiture->marque = 'audi';
-$voiture->temps = 2;
+static function getUnite()
+{
+    return "km";
+}
+}
+//$voiture = new Voiture();
+
+echo Voiture::getInfo();
+//Une méthode statique est une méthode qui 
+//ne necessite pas l'instanciation d'une classe pour y acceder
+
+
+//////////////////////////////////////////////////
+
+//}
+//$voiture = new Voiture ();
+//$voiture->vitesse = 55;
+//$voiture->marque = 'audi';
+//$voiture->temps = 2;
 
 //echo "<pre>";   
 //var_dump($voiture);
@@ -27,14 +44,16 @@ $voiture->temps = 2;
 //var_dump($voiture->vitesse, $voiture->marque);
 //echo "</pre>";
 
-var_dump($voiture->calculerDistance());
+//var_dump($voiture->calculerDistance());
 
-die;
+//die;
 
-$voiture2 = new Voiture ();
-$voiture2->vitesse = 25;
-$voiture2->marque = 'bmw';
-$voiture2->temps = 2;
-echo "<pre>";
-var_dump($voiture2->vitesse, $voiture2->marque);
-echo "</pre>";
+//$voiture2 = new Voiture ();
+//$voiture2->vitesse = 25;
+//$voiture2->marque = 'bmw';
+//$voiture2->temps = 2;
+//echo "<pre>";
+//var_dump($voiture2->vitesse, $voiture2->marque);
+//echo "</pre>";
+
+
