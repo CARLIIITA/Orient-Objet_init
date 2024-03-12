31e2c95e0639289249c4d8f288d5cfaf4dd67fb3
unknown <carla.elatre@gmail.com>
2024-03-12 16:53:52 +0100

    commit

---
+++ HeritageExo.php
@@ -0,0 +1,49 @@
+<?php 
+
+//exercice 2 Héritage
+//Créez une classe abstraite Forme avec une méthode abstraite calculerAire().
+//Créez trois classes dérivées : Cercle, Rectangle et Triangle.
+//Chaque classe doit étendre la classe abstraite Forme et implémenter 
+//la méthode calculerAire() en fonction de la forme géométrique correspondante.
+//Assurez-vous d'ajouter les propriétés nécessaires à chaque classe 
+//pour stocker les dimensions spécifiques de chaque forme 
+//(rayon pour le cercle, longueur et largeur pour le rectangle, etc.).
+//Implémentez la méthode magique __clone() dans les classes Cercle, Rectangle et Triangle.
+//Personnalisez le comportement de clonage pour chaque classe en fonction de ses propriétés.
+
+//Créez des objets de chaque classe représentant différentes formes géométriques.
+//Clônez certains de ces objets pour créer des copies.
+//Affichez les propriétés et les aires des formes d'origine et de leurs copies clonées.
+//Vérification des résultats :
+
+//Assurez-vous que les copies clonées ont les mêmes propriétés que les objets d'origine.
+//Vérifiez que les aires des formes d'origine et de leurs copies clonées sont identiques.
+//calcule de l'air d'un cercle : pi x rayon x rayon 
+//sur php la fonction 
+//pi()
+//retourne la valeur de pi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
